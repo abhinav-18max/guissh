ba5f8f39e031b6ae33440c3578340a6fbc9c3f60
Rahul Krishna K A <rahulkrishna7602@gmail.com>
2023-09-09 02:23:46 +0530

    password based set

--- main.go
+++ main.go
@@ -2,21 +2,22 @@ package main
 
 import (
 	"fmt"
-	"github.com/joho/godotenv"
-	"github.com/melbahja/goph"
+	// "github.com/joho/godotenv"
 	"log"
-	
+
+	"github.com/melbahja/goph"
 )
 
 func main() {
-	godotenv.Load(".env")
-	auth, err := goph.Key("./aksc.pem", "")
+	// godotenv.Load(".env")
+	// auth, err := goph.Key("/home/rahul/.ssh/id_rsa", )
 
-	if err != nil {
-		log.Fatal(err)
-	}
+	auth := goph.Password("D#YPb4chIp")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	client, err := goph.New("root","3.210.248.187", auth)
+	client, err := goph.New("root", "74.220.20.200", auth)
 
 	if err != nil {
 		log.Fatal(err)
